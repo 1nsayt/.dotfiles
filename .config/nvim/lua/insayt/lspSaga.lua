c1b394e4f49ecbcf5274caa19c6dd8971985563f
1nsayt <1nsayt@mail.ru>
2022-11-13 18:07:23 +0500

    rewritten lsp commands by lspSaga

--- .config/nvim/lua/insayt/lspSaga.lua
+++ .config/nvim/lua/insayt/lspSaga.lua
@@ -3,48 +3,30 @@ local saga = require('lspsaga')
 
 saga.init_lsp_saga()
 
--- Lsp finder find the symbol definition implement reference
--- if there is no implement it will hide
--- when you use action in finder like open vsplit then you can
 -- use <C-t> to jump back
-keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
-
--- Code action
-keymap({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
-
--- Rename
-keymap("n", "gr", "<cmd>Lspsaga rename<CR>", { silent = true })
-
--- Peek Definition
--- you can edit the definition file in this flaotwindow
--- also support open/vsplit/etc operation check definition_action_keys
+keymap("n", "<leader>grr", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+keymap({"n","v"}, "<leader>gca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+keymap("n", "<leader>grn", "<cmd>Lspsaga rename<CR>", { silent = true })
 -- support tagstack C-t jump back
-keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
-
+keymap("n", "<leader>gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
 -- Show line diagnostics
-keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
-
+keymap("n", "<leader>gsd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
 -- Show cursor diagnostic
-keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
-
+keymap("n", "<leader>gsd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
 -- Diagnsotic jump can use `<c-o>` to jump back
-keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
-keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
-
+keymap("n", "<leader>gp", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
+keymap("n", "<leader>gn", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
 -- Only jump to error
-keymap("n", "[E", function()
+keymap("n", "<leader>gP", function()
   require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
 end, { silent = true })
-keymap("n", "]E", function()
+keymap("n", "<leader>gN", function()
   require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
 end, { silent = true })
-
 -- Outline
-keymap("n","<leader>o", "<cmd>LSoutlineToggle<CR>",{ silent = true })
-
+keymap("n","<leader>go", "<cmd>LSoutlineToggle<CR>",{ silent = true })
 -- Hover Doc
-keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
-
+keymap("n", "<leader>gh", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 -- Float terminal
 keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
 -- if you want pass somc cli command into terminal you can do like this
--- .config/nvim/plugin/lsp.vim
+++ .config/nvim/plugin/lsp.vim
@@ -5,16 +5,8 @@ fun! LspLocationList()
     " lua vim.lsp.diagnostic.set_loclist({open_loclist = false})
 endfun
 
-nnoremap <leader>gd :lua vim.lsp.buf.definition()<CR>
 nnoremap <leader>gi :lua vim.lsp.buf.implementation()<CR>
 nnoremap <leader>gsh :lua vim.lsp.buf.signature_help()<CR>
-nnoremap <leader>grr :lua vim.lsp.buf.references()<CR>
-nnoremap <leader>grn :lua vim.lsp.buf.rename()<CR>
-nnoremap <leader>gh :lua vim.lsp.buf.hover()<CR>
-nnoremap <leader>gca :lua vim.lsp.buf.code_action()<CR>
-nnoremap <leader>gsd :lua vim.lsp.diagnostic.show_line_diagnostics()<CR>
-nnoremap <leader>gn :lua vim.lsp.diagnostic.goto_next()<CR>
-nnoremap <leader>gll :call LspLocationList()<CR>
 
 let g:compe = {}
 let g:compe.enabled = v:true
